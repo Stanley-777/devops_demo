464432b6b4fea915d2b932058f5d517a7fc831c6
stanley-777 <zstanleyhenry@gmail.com>
2023-09-01 11:04:05 +0530

    new file

---
+++ src/main/java/com/mycompany/app/app2.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class app2
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "tabley" );
+    }
+}
